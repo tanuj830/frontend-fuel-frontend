14f5ceeacfb4a39c17c9d9ba2c6cf7ed4ba6a922
Tanuj Bhatt <tanujbhatt644@gmail.com>
2025-05-03 16:19:12 +0530

    seo: sitemap added

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://greatreact.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  

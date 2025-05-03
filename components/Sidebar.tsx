145ed0377c89b1cdc29a3602e0ea64105095b293
Tanuj Bhatt <tanujbhatt644@gmail.com>
2025-05-03 05:45:17 +0530

    added sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,23 +17,23 @@ const Sidebar = ({layout, setLayout}:any) => {
 <div className=' flex flex-col gap-1 pt-5 pr-5 pl-5'>
   <div className=''>
       <button onClick={()=>setLayout("dashboard-layout")} className={`hover:bg-muted cursor-pointer w-full text-start text-[13px] rounded-lg text-muted-foreground py-2 px-2 flex items-center gap-2
-${layout === "dashboard-layout" && "bg-muted text-primary-foreground"}`}><LayoutDashboard width={16} height={16}/> Dashboard</button>
+${layout === "dashboard-layout" && "bg-muted text-secondary-foreground"}`}><LayoutDashboard width={16} height={16}/> Dashboard</button>
   </div>
   <div className=''>
       <button onClick={()=>setLayout("questions-layout")} className={`hover:bg-muted cursor-pointer w-full text-start text-[13px] rounded-lg text-muted-foreground py-2 px-2 flex items-center gap-2
-${layout === "questions-layout" && "bg-muted text-primary-foreground"}`}><ListChecks width={16} height={16}/> All practice questions</button>
+${layout === "questions-layout" && "bg-muted text-secondary-foreground"}`}><ListChecks width={16} height={16}/> All practice questions</button>
   </div>
   <div className=''>
       <button onClick={()=>setLayout("uicoding-layout")} className={`hover:bg-muted cursor-pointer w-full text-start text-[13px] rounded-lg text-muted-foreground py-2 px-2 flex items-center gap-2
-${layout === "uicoding-layout" && "bg-muted text-primary-foreground"}`}><SiReact width={16} height={16}/> UI questions</button>
+${layout === "uicoding-layout" && "bg-muted text-secondary-foreground"}`}><SiReact width={16} height={16}/> UI questions</button>
   </div>
   <div className=''>
       <button onClick={()=>setLayout("algocoding-layout")} className={`hover:bg-muted cursor-pointer w-full text-start text-[13px] rounded-lg text-muted-foreground py-2 px-2 flex items-center gap-2
-${layout === "algocoding-layout" && "bg-muted text-primary-foreground"}`}><Brain width={16} height={16}/> Algorithms</button>
+${layout === "algocoding-layout" && "bg-muted text-secondary-foreground"}`}><Brain width={16} height={16}/> Algorithms</button>
   </div>
   <div className=''>
       <button onClick={()=>setLayout("jscoding-layout")} className={`hover:bg-muted cursor-pointer w-full text-start text-[13px] rounded-lg text-muted-foreground py-2 px-2 flex items-center gap-2
-${layout === "jscoding-layout" && "bg-muted text-primary-foreground"}`}><AiOutlineJavaScript width={16} height={16}/> Javascript questions</button>
+${layout === "jscoding-layout" && "bg-muted text-secondary-foreground"}`}><AiOutlineJavaScript width={16} height={16}/> Javascript questions</button>
   </div>
 </div>
 

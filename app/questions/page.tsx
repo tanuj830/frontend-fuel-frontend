efc76a224d6b739b543d99df26f40aa24b64c644
Tanuj Bhatt <tanujbhatt644@gmail.com>
2025-05-03 17:14:48 +0530

    seo: sitemap added

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -33,10 +33,6 @@ const Questions = () => {
           };
       
           fetchQuestions();
-        // axios.get("/api/questions").then(res => setQuestions(res.data)).catch(err=>console.log(err))
-        // axios.get(`${BASE_URL}/api/questions`).then(res => setQuestions(res.data)).catch(err=>console.log(err))
-        // axios.get(`${BASE_URL}/api/questions`).then(res => setQuestions(res.data)).catch(err=>console.log(err))
-
     }, [])
 
     return (
@@ -61,3 +57,59 @@ const Questions = () => {
 }
 
 export default Questions
+
+
+
+// Remove "use client"; it's not needed for SSR pages
+// import { supabase } from '@/lib/supabaseClient';
+// import Sidebar from '@/components/Sidebar';
+// import QuestionsLayout from '@/components/layouts/QuestionsLayout';
+// import JSCodingLayout from '@/components/layouts/JSCodingLayout';
+// import AlgoCodingLayout from '@/components/layouts/AlgoCodingLayout';
+// import UICodingLayout from '@/components/layouts/UICodingLayout';
+// import DashboardLayout from '@/components/layouts/DashboardLayout';
+// import Navbar from '@/components/Navbar';
+// import React, { useState } from 'react';
+
+// export default function Questions({ initialQuestions }:any) {
+//   const [questions, setQuestions] = useState(initialQuestions);
+//   const [layout, setLayout] = useState("dashboard-layout");
+
+//   return (
+//     <>
+//       <Navbar layout={layout} setLayout={setLayout} />
+//       <div className='lg:flex'>
+//         <div className='relative w-[25vw] min-h-[70vh] border-r hidden lg:inline-block'>
+//           <Sidebar layout={layout} setLayout={setLayout} />
+//         </div>
+
+//         {/* Render layouts */}
+//         {
+//           layout === "questions-layout" ? <QuestionsLayout questions={questions} /> :
+//           layout === "dashboard-layout" ? <DashboardLayout questions={questions} /> :
+//           layout === "uicoding-layout" ? <UICodingLayout /> :
+//           layout === "algocoding-layout" ? <AlgoCodingLayout /> :
+//           layout === "jscoding-layout" ? <JSCodingLayout /> : null
+//         }
+//       </div>
+//     </>
+//   );
+// }
+
+// // 🔽 Fetching data on the server side
+// export async function getServerSideProps() {
+//   const { data, error } = await supabase
+//     .from('questions')
+//     .select('*');
+
+//   if (error) {
+//     console.error('Error fetching questions:', error);
+//     return { props: { initialQuestions: [] } };
+//   }
+
+//   return {
+//     props: {
+//       initialQuestions: data,
+//     },
+//   };
+// }
--- components/layouts/AlgoCodingLayout.tsx
+++ components/layouts/AlgoCodingLayout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect } from 'react'
 import Navbar from '@/components/Navbar'
 import QuestionsPlayground from '@/components/QuestionsPlayground'

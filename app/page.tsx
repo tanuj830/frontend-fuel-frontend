e4746f3ca65b7c5e403ed7b9a48584765628d271
Tanuj Bhatt <tanujbhatt644@gmail.com>
2025-05-05 16:28:54 +0530

    profile page added

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function App() {
         <meta name="keywords" content="React, UI coding, Algo coding, JavaScript questions, frontend interview prep" />
         <meta property="og:title" content="GreatReact | UI & Algorithmic Coding Questions" />
         <meta property="og:description" content="A platform to solve and build UI coding challenges and algorithmic problems." />
-        <meta property="og:url" content="https://greatreact.com/" />
+        <meta property="og:url" content="https://greatreact.com" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/logo.svg" />
         {/* <meta name="twitter:card" content="summary_large_image" /> */}

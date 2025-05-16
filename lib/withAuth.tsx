6555db9cf97b29d7bfb31cef78d57480fc292564
Tanuj Bhatt <tanujbhatt644@gmail.com>
2025-05-16 16:48:19 +0530

    tailwind integration with react ide

--- lib/withAuth.tsx
+++ lib/withAuth.tsx
@@ -1,29 +1,29 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/components/AuthContext";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// // import { useAuth } from "@/components/AuthContext";
 
-const withAuth = (Component: any) => {
+// const withAuth = (Component: any) => {
 
    
   
-  return function ProtectedComponent(props: any) {
-    const { user, loading } = useAuth();
-    const router = useRouter();
+//   return function ProtectedComponent(props: any) {
+//     const { user, loading } = useAuth();
+//     const router = useRouter();
 
-    useEffect(() => {
-      if (!loading && !user) {
-        router.push("/sign-in");
-      }
-    }, [user, loading]);
+//     useEffect(() => {
+//       if (!loading && !user) {
+//         router.push("/sign-in");
+//       }
+//     }, [user, loading]);
 
-    // 👉 Only block rendering if loading is true
-    if (loading) return <div className="p-4">Checking session...</div>; // Optional loader
-    if (!user) return null; // user is null after loading, redirecting
+//     // 👉 Only block rendering if loading is true
+//     if (loading) return <div className="p-4">Checking session...</div>; // Optional loader
+//     if (!user) return null; // user is null after loading, redirecting
 
-    return <Component {...props} />;
-  };
-};
+//     return <Component {...props} />;
+//   };
+// };
 
-export default withAuth;
+// export default withAuth;
